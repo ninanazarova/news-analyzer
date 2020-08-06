dde0f04c5964a1e0321cd28c9c2b9a730734b06e
Nina Nazarova <ninanazarova0496@yandex.ru>
2020-08-06 17:30:13 +0300

    about page adaptive

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,1 +1,61 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+Swiper.use([Navigation, Pagination]);
+import "swiper/swiper-bundle.css";
 import "./about.css";
+
+const mySwiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  effect: "slide",
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: "auto",
+      spaceBetween: 8,
+      centeredSlides: true,
+      loop: false,
+    },
+    450: {
+      slidesPerView: "auto",
+      spaceBetween: 8,
+      centeredSlides: true,
+      loop: true,
+    },
+    500: {
+      loop: true,
+      spaceBetween: 8,
+      slidesPerView: "auto",
+      centeredSlides: true,
+    },
+    640: {
+      slidesPerView: "auto",
+      spaceBetween: 8,
+      centeredSlides: true,
+      loop: true,
+    },
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 8,
+      centeredSlides: false,
+      loop: false,
+    },
+    1024: {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      centeredSlides: true,
+    },
+  },
+});
